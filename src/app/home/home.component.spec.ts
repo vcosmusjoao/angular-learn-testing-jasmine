aa4f87edf53c357f6621b83e607f8a5f0aaff339
João Victor <joao.victor@sinqia.com.br>
2024-07-30 12:47:05 -0300

    Aprendendo a como testar DOM Element usando jasmine.

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,10 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let el: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -16,10 +19,23 @@ describe('HomeComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
+    el = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  it('should have a paragraph', () => {
+    let pElements = el.queryAll(By.css('p'));
+    expect(pElements[0].nativeElement.textContent).toBe('home works!')
+  });
+
+  it('should have a disabled button', () => {
+   let buttonElements = el.queryAll(By.css('.btn'));
+   expect(buttonElements[0].nativeElement.disabled).toBeTrue();
+  });
+
 });
